a2e3e44f1cdec2be2b2de1a87d770ddba3788e54
alikbader <alikbader@gmail.com>
2021-12-29 07:44:35 +0200

    threads addition
    now the server creates threads for each broadcasting and accepting new players, and stops broadcasting whenever it accepts enough players

--- client.py
+++ client.py
@@ -4,28 +4,26 @@ from socket import *
 from struct import *
 
 
-def udpConnection() :
+def udpConnection():
     udp_socket = socket(AF_INET, SOCK_DGRAM)
-    udp_socket.setsockopt(SOL_SOCKET,SO_BROADCAST,1)
+    udp_socket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
     udp_socket.bind(('', 13117))
-    msg, address= udp_socket.recvfrom(1024)
-    magic_cookies , msg_type , port = struct.unpack('!IBH',msg)
+    msg, address = udp_socket.recvfrom(1024)
+    magic_cookies, msg_type, c_port = struct.unpack('!IBH', msg)
     print("Client started, listening for offer requests...")
-    if magic_cookies == 0xabcddcba and msg_type == 0x2 :
-        address_server=address[0]
-        print("Received offer from ",address_server,",attempting to connect...")
-    return port,address_server
+    if magic_cookies == 0xabcddcba and msg_type == 0x2:
+        server_address = address[0]
+        print("Received offer from ", server_address, ",attempting to connect...")
+        return c_port, server_address
 
-def tcpConnection(port,address_server):
-    tcp_socket=socket(AF_INET, SOCK_STREAM)
-    time.sleep(10)
-    print(port , address_server)
-    tcp_socket.connect((address_server, port))
+
+def tcpConnection(c_port, server_address):
+    tcp_socket = socket(AF_INET, SOCK_STREAM)
+    # time.sleep(10)
+    print(c_port, server_address)
+    tcp_socket.connect((server_address, c_port))
     tcp_socket.send("nadia\n".encode('utf-8'))
 
 
-
-
-
-port,address_server =udpConnection()
-tcpConnection(port,address_server)
+port, address_server = udpConnection()
+tcpConnection(port, address_server)
--- server.py
+++ server.py
@@ -1,33 +1,46 @@
 import time
 from socket import *
 from struct import *
+import threading
 
 ip = gethostname()
+enough_players = False
 
-def udpConnection():
 
+def UDPConnection():
+    global enough_players
     udp_socket = socket(AF_INET, SOCK_DGRAM)
-    udp_socket.setsockopt(SOL_SOCKET,SO_BROADCAST,1)
-    msg = pack('!IBH',0xabcddcba , 0x2 , 2122)
-    timenow = time.time()
+    udp_socket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+    msg = pack('!IBH', 0xabcddcba, 0x2, 2122)
+
     print("Server started, listening on IP address ", ip)
-    while time.time() <= timenow + 10:
+
+    end_time = time.time() + 10
+    while time.time() <= end_time and not enough_players:
         udp_socket.sendto(msg, ('<broadcast>', 13117))
         time.sleep(1)
-        print(time.time())
+        print('tick')
 
-def tcpconnection():
+
+def TCPConnection():
+    global enough_players
     tcp_socket = socket(AF_INET, SOCK_STREAM)
     tcp_socket.bind((ip, 2122))
     tcp_socket.listen()
+
     socket1, address1 = tcp_socket.accept()
     socket2, address2 = tcp_socket.accept()
+
+    enough_players = True
+
     msg1 = socket1.recv(1024)
     msg2 = socket2.recv(1024)
-    # msg, address = tcp_socket.accept()
-    print(msg1.decode())
-    print(msg2.decode())
 
 
-udpConnection()
-tcpconnection()
+# create threads for broadcasting and accepting players
+players_conn_thread = threading.Thread(target=TCPConnection, args=())
+broadcast_thread = threading.Thread(target=UDPConnection, args=())
+
+players_conn_thread.start()
+broadcast_thread.start()
+
